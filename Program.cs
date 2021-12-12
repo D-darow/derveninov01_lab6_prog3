89819056b7962d8a39a1d54f49cef98d9d32f25a
Dima <dimader@bk.ru>
2021-12-12 22:20:29 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace derveninov01_lab6_prog3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
