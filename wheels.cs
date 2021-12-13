927526abcd46455cd2d9db6d62d532a1bc3fa435
Dima <dimader@bk.ru>
2021-12-14 04:06:53 +0700

    Исправлены свойства для полей в wheels.cs

--- wheels.cs
+++ wheels.cs
@@ -9,9 +9,19 @@ namespace derveninov01_lab6_prog3
     public struct wheels
     {
 		// Диаметр колеса (В дюймах)
-		private int diameter_of_disk { get; init; }
+		private int diameter_of_disk;
+		public int _diameter_of_disk
+		{
+			get { return diameter_of_disk; }
+			set { diameter_of_disk = value; }
+		}
 		// Ширина шины (В мм)
-		private int width_of_tire { get; init; }
+		private int width_of_tire;
+		public int _width_of_tire
+		{
+			get { return width_of_tire; }
+			set { width_of_tire = value; }
+		}
 
 		// Конструктор
 		public wheels(int diametr_of_disk, int width_of_tire)
