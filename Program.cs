c824ceaf5fb69b389630391178730d5bdf02d4a7
Dima <dimader@bk.ru>
2021-12-17 19:42:41 +0700

    Демонстрация работы с двумерным массивом объектов класса

--- Program.cs
+++ Program.cs
@@ -6,26 +6,25 @@ namespace derveninov01_lab6_prog3
     {
         static void Main(string[] args)
         {
-			bus my_bus = new bus();
-			my_bus.add_passenger();
-			try
-			{
-				int check = my_bus.remove_passenger();
-				if (check == -1) throw new Exception("\nАвтобус пуст.\n");
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.Message);
-			}
-			try
-			{
-				int check = my_bus.remove_passenger();
-				if (check == -1) throw new Exception("\nАвтобус пуст.\n");
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.Message);
-			}
-		}
+            bus[,] bus_array = new bus[2,2];
+            int bus_num = 1;
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    bus_array[i,j] = new bus(bus_num);
+                    bus_num++;
+                }
+            }
+            bus_array[0,0]._price = 25;
+            bus_array[0,0].add_passenger();
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    bus_array[i,j].print_bus();
+                }
+            }
+        }
     }
 }
