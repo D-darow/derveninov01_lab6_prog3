c5b052f78433c4da3a3b09fc6f0eb9c6d319abb8
Dima <dimader@bk.ru>
2021-12-18 18:00:12 +0700

    Добавлен абстрактный класс Фигура и производный от него Круг

---
+++ Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Пример интерфейса
+interface Printable
+{
+	void print();
+}
+namespace derveninov01_lab6_prog3
+{
+    class Circle : Figure, Printable
+    {
+		private double radius;
+		public Circle(double r)
+		{
+			radius = r;
+		}
+		// Площадь
+		public override double getSquare()
+		{
+			return radius * radius * 3.14;
+		}
+		// Периметр
+		public override double getPerimeter()
+		{
+			return 2 * 3.14 * radius;
+		}
+		// Печать
+		public void print()
+		{
+			Console.WriteLine("Радиус круга = " + radius);
+		}
+	}
+}
---
+++ Figure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace derveninov01_lab6_prog3
+{
+    abstract class Figure
+    {
+        // абстрактный метод для получения периметра
+        public abstract double getPerimeter();
+        // абстрактный метод для получения площади
+        public abstract double getSquare();
+    }
+}
