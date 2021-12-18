df19906129648300767d44ec1400eb05c1293504
Dima <dimader@bk.ru>
2021-12-18 18:00:33 +0700

    Модифицирован класс Program для демонстрации изменений

--- Program.cs
+++ Program.cs
@@ -6,25 +6,13 @@ namespace derveninov01_lab6_prog3
     {
         static void Main(string[] args)
         {
-            bus[,] bus_array = new bus[2,2];
-            int bus_num = 1;
-            for (int i = 0; i < 2; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    bus_array[i,j] = new bus(bus_num);
-                    bus_num++;
-                }
-            }
-            bus_array[0,0]._price = 25;
-            bus_array[0,0].add_passenger();
-            for (int i = 0; i < 2; i++)
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    bus_array[i,j].print_bus();
-                }
-            }
+            foreign_driver new_driver = new foreign_driver("Кто", "То", "967565678", 4325);
+            // Глубокое клонирование
+            foreign_driver clone_driver = (foreign_driver) new_driver.Clone();
+            clone_driver.print_driver();
+            // Производный класс от абстрактного Figure
+            Circle my_circle = new Circle(2.345);
+            Console.WriteLine("\nПлощадь круга = " + my_circle.getSquare());
         }
     }
 }
